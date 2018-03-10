be0f081eb5393234868f3d07fe02e442920c7d4e
JJ D'Aurora <jjdaurora@gmail.com>
2018-03-10 01:59:45 -0500

    data scraping and handlebars complete

--- server.js
+++ server.js
@@ -24,76 +24,88 @@ app.use(express.static("public"));
 
 mongoose.Promise = Promise;
 
-var MONGODB_URI =
-  process.env.MONGODB_URI ||("mongodb://localhost/mongoHeadlines",
-  {
-    useMongoClient: true
-  });
+mongoose.connect("mongodb://localhost/mongoHeadlines") ;
 
 // Routes
 
 app.get("/scrape", function(req, res) {
-  axios.get("http://www.theverge.com").then(function(response) {
+  // First, we grab the body of the html with request
+  axios.get("http://www.echojs.com/").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
-    $("h2.c-entry-box--compact__title").each(function(i, element) {
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h2").each(function(i, element) {
+      // Save an empty result object
       var result = {};
-
-      result.title = $(element).children("a").attr();
-
-      result.author = $(element).children("a").attr();
-
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.author = $(this)
+        .children("a")
+        .attr("href");
+      // Create a new Article using the `result` object built from scraping
       db.Headline.create(result)
         .then(function(dbHeadline) {
-          res.send("Scrape Complete");
+
+          Headline: {
+            title: result
+            author: result
+          }
+          // View the added result in the console
+          console.log(dbHeadline);
         })
-        .catch(function(err) {
-          res.json(err);
-        });
+        // .catch(function(err) {
+        //   // If an error occurred, send it to the client
+        //   return res.json(err);
+        // });
     });
+    // If we were able to successfully scrape and save an Article, send a message to the client
+    res.send("Scrape Complete");
   });
 });
 
 app.get("/headlines", function(req, res) {
   db.Headline.find({})
-    .then(function(dbHeadline) {
-      res.render("Headline", {
-        title: res,
-        author: res
-      });
+    .then(function(data) {
+      var headlineData = {
+        Headline: data
+      };
+      res.render("saved", headlineData);
     })
     .catch(function(err) {
       res.json(err);
     });
 });
 
-app.get("/articles/:id", function(req, res) {
-  let id = req.params.id;
+// app.get("/articles/:id", function(req, res) {
+//   let id = req.params.id;
 
-  db.Article.find({ _id: id })
-    .then(function(dbHeadline) {
-      res.json(dbHeadline);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
+//   db.Article.find({ _id: id })
+//     .then(function(dbHeadline) {
+//       res.json(dbHeadline);
+//     })
+//     .catch(function(err) {
+//       res.json(err);
+//     });
+// });
 
-app.post("/articles/:id", function(req, res) {
-  db.Note.create(req.body)
-    .then(function(dbNote) {
-      return db.Article.findOneAndUpdate(
-        { _id: req.params.id },
-        { note: dbNote._id },
-        { new: true }
-      );
-    })
-    .then(function(dbHeadline) {
-      res.json(dbHeadline);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
+// app.post("/articles/:id", function(req, res) {
+//   db.Note.create(req.body)
+//     .then(function(dbNote) {
+//       return db.Article.findOneAndUpdate(
+//         { _id: req.params.id },
+//         { note: dbNote._id },
+//         { new: true }
+//       );
+//     })
+//     .then(function(dbHeadline) {
+//       res.json(dbHeadline);
+//     })
+//     .catch(function(err) {
+//       res.json(err);
+//     });
+// });
 
 // Start the server
 app.listen(PORT, function() {
